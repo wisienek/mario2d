249c17239ff8b1c81ea780dace93fa149cb86afd
Wisienek <frascowoolf@gmail.com>
2021-11-17 21:33:05 +0100

    Mario -> movement && collision

--- Mario.cpp
+++ Mario.cpp
@@ -1,11 +1,9 @@
 #include "Mario.h"
+#include <iostream>
+
 
 Mario::Mario( float X, float Y ) {
-	// ustaw podstawow¹ pozycjê
-	shape.setPosition(X, Y);
-	texture.loadFromFile(textureFile_small_idle);
-
-	shape.setTexture(texture);
+	init(X, Y);
 }
 
 // const - nie zmienia pól klasy
@@ -14,27 +12,141 @@ void Mario::draw(RenderTarget& target, RenderStates state) const {
 }
 
 void Mario::update() {
+	this->updateKeyInput();
 
-	// check for colision
+	if (jumping == 1) {
+		this->move(0.f, -this->playerVelocity * 1.3);
+		jumpingCounter += this->playerVelocity;
 
-	if (this->left() + velocity.x < 0)
+		if (jumpingCounter > jumpingDuration) {
+			endJumping();
+		}
+	}
+	else if (jumping == -1) {
+		this->move(0.f, this->playerVelocity * 1.7);
+	}
+}
+
+void Mario::updateKeyInput()
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+		this->move(-this->playerVelocity, 0.f);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+		this->move(this->playerVelocity, 0.f);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+		this->jump();
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+		this->crouch();
+	}
+}
+
+void Mario::init(float X, float Y)
+{
+	this->shape.setPosition(X, Y);
+	this->texture.loadFromFile(textureFile_small_idle);
+	this->shape.setTexture(texture);
+}
+
+sf::FloatRect Mario::bounds()
+{
+	return this->shape.getGlobalBounds();
+}
+
+void Mario::move(float x, float y)
+{
+	this->prevPos = { this->shape.getPosition().x, this->shape.getPosition().y };
+
+	this->shape.move(x, y);
+	resolveColision();
+}
+
+void Mario::setPosition(float x, float y)
+{
+	this->shape.setPosition(x, y);
+}
+
+void Mario::jump()
+{
+	if (jumping != 0) return;
+
+	startJumping();
+	if (jumping == 0) {
+		// play sound
+	}
+}
+
+void Mario::crouch()
+{
+}
+
+void Mario::startJumping()
+{
+	if (this->jumping == 0) {
+		jumping = 1;
+	}
+}
+
+void Mario::endJumping()
+{
+	if (jumping)
+	{
+		jumpingCounter = 0;
+		jumping = -1;
+	}
+}
+
+void Mario::setVideoBounds(float w, float h)
+{
+	this->boundsW = w;
+	this->boundsH = h;
+}
+
+void Mario::resolveColision()
+{
+	if (canCollide == false) return;
+
+	bool goBack = false;
+
+	// loop through every object
+	
+
+
+	// check for border col
+	sf::FloatRect bounds = this->bounds();
+	if (bounds.left <= 0
+		|| bounds.left + bounds.width >= this->boundsW
+		|| bounds.top <= 0
+		|| bounds.top + bounds.height >= this->boundsH)
+		goBack = true;
+
+	// stop falling
+	if (this->jumping == -1 && bounds.top + bounds.height >= this->boundsH)
+		this->jumping = 0;
+
+	if (goBack)
+		this->shape.setPosition(this->prevPos);
+}
+
+void Mario::hit(Object * what)
+{
+}
+
+void Mario::hurt()
+{
+	if (this->hearts - 1 < 0) {
+		this->die();
 		return;
-
-	shape.move( velocity );
+	}
+	this->hearts -= 1;
 }
 
-float Mario::left() {
-	return this->shape.getPosition().x - (this->width/ 2);
+void Mario::die()
+{
 }
 
-float Mario::right() {
-	return this->shape.getPosition().x + (this->width / 2);
-}
-
-float Mario::top() {
-	return this->shape.getPosition().y + (this->height / 2);
-}
-
-float Mario::bottom() {
-	return this->shape.getPosition().y - (this->height / 2);
+void Mario::animate()
+{
 }
--- Mario.h
+++ Mario.h
@@ -1,10 +1,11 @@
 #pragma once
-#include <SFML/Graphics.hpp>
+
+#include "Object.h"
 
 using namespace sf;
 
 // Mario extends SFML:: Drawable <- drawable object
-class Mario : public sf::Drawable
+class Mario : public Object
 {
 private:
 	// sprite config
@@ -21,7 +22,7 @@ private:
 	String textureFile_small_hang_0 = "./resources/graphics/entities/mario-small-hang-0.png";
 	String textureFile_small_hang_1 = "./resources/graphics/entities/mario-small-hang-1.png";
 
-
+	Vector2f prevPos{0, 0};
 	// small mario 16x16px
 	const short int small_width{ 16 };
 	const short int small_height{ 16 };
@@ -31,13 +32,24 @@ private:
 	// current size
 	short int width{ small_height };
 	short int height{ small_height };
+	float boundsW;
+	float boundsH;
 
 	// class config
+	bool isBig = false;
+	bool canFire = false;
+	bool isBouncing = false;
+	bool isRunning = false;
+	bool isCrouching = false;
+	bool canCollide = true;
+	
 	short int hearts = 3;
 	// movement
-	const float playerVelocity{ 2.0f };
+	const float playerVelocity{ 1.3f };
 	// -1 <- down, 0 <- standing, 1 <- up
 	short int jumping{ 0 };
+	short int jumpingDuration{ 60 };
+	short int jumpingCounter{ 0 };
 	// -1 <- left, 0 <- standing, 1 <- right
 	short int direction{ 0 };
 	// moving somehere ( can be (0;0) if not moving )
@@ -47,20 +59,39 @@ private:
 	void draw(RenderTarget& target, RenderStates state) const override;
 
 	// movement
-	void update();
-
+	void updateKeyInput();
+	void init(float X, float Y);
 public:
 	// x,y - default position
-	Mario( float X, float Y );
+	Mario( float X=0, float Y=0 );
 
 	// destructuring
 	Mario() = delete;
 	~Mario() = default;
 
+	virtual std::string name() { return "Mario"; };
+	virtual void update();
+
 	// positions
-	float left();
-	float right();
-	float top();
-	float bottom();
+	sf::FloatRect bounds();
+	void move( float x, float y );
+	void setPosition(float x, float y);
+	
+	void jump();
+	void crouch();
+	// can be in base class of entity
+	void startJumping(); 
+	void endJumping();
+
+	// collisions
+	void setVideoBounds(float w, float h);
+	virtual void resolveColision();
+
+	virtual void hit(Object *what);
+	virtual void hurt();
+	virtual void die();
+
+	//animation
+	virtual void animate();
 };
 
