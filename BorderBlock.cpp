018e44649c31a70ad68068835695b04c522cb67e
Mwisni <40739949+wisienek@users.noreply.github.com>
2021-12-05 22:55:44 +0100

    BB - init v2i

--- BorderBlock.cpp
+++ BorderBlock.cpp
@@ -8,6 +8,17 @@ BorderBlock::~BorderBlock()
 {
 }
 
+void BorderBlock::init(sf::Vector2i v)
+{
+	this->walkable = true;
+	this->collectable = false;
+	this->collidable = true;
+
+	this->blockTexture.loadFromFile(this->blockTextureSrc);
+	this->shape.setTexture(this->blockTexture);
+	this->shape.setPosition(v.x, v.y);
+}
+
 void BorderBlock::init(float x, float y)
 {
 	this->walkable = true;
--- BorderBlock.h
+++ BorderBlock.h
@@ -15,6 +15,7 @@ public:
 	virtual void hurt() {};
 	virtual void die() {};
 
+	void init(sf::Vector2i v);
 	void init(float x=0, float y=0);
 	virtual void hit() {};
 	virtual void animate(float deltaTime) {}
