c30df244e63b14b5b25336e95d03681502d1b5c9
Mwisni <40739949+wisienek@users.noreply.github.com>
2022-02-12 21:24:21 +0100

    sound update

--- SoundManager.cpp
+++ SoundManager.cpp
@@ -14,15 +14,77 @@ SoundManager* SoundManager::getInstance()
 
 void SoundManager::initSounds()
 {
+	// gameover sound
+	if (!this->gameoverBuffer.loadFromFile("./resources/sounds/gameover.wav"))
+		std::cout << "Couldn't load gameover sound!" << std::endl;
+
+	this->gameoverSound.setBuffer(this->gameoverBuffer);
+	this->gameoverSound.setVolume(3.f);
+
+	// dying sound
+	if (!this->dyingBuffer.loadFromFile("./resources/sounds/death.wav"))
+		std::cout << "Couldn't load dying sound!" << std::endl;
+
+	this->dyingSound.setBuffer(this->dyingBuffer);
+	this->dyingSound.setVolume(3.f);
+
+	// shrinking sound
+	if (!this->shrinkBuffer.loadFromFile("./resources/sounds/shrink.wav"))
+		std::cout << "Couldn't load shrink sound!" << std::endl;
+
+	this->shrinkSound.setBuffer(this->shrinkBuffer);
+	this->shrinkSound.setVolume(2.f);
+
+	// jump sound
 	if (!this->jumpingBuffer.loadFromFile("./resources/sounds/jump-small.wav"))
 		std::cout << "Couldn't load junping sound!" << std::endl;
 
 	this->jumpingSound.setBuffer(this->jumpingBuffer);
 	this->jumpingSound.setVolume(2.f);
 
+	// coin sound
+	if (!this->coinBuffer.loadFromFile("./resources/sounds/coin.wav"))
+		std::cout << "Couldn't load coin sound!" << std::endl;
+
+	this->coingSound.setBuffer(this->coinBuffer);
+	this->coingSound.setVolume(2.5f);
+
+	// goomba died sound
+
+	if (!this->goombadiedBuffer.loadFromFile("./resources/sounds/stomp.wav"))
+		std::cout << "Couldn't load goomba dying sound!" << std::endl;
+
+	this->goombadiedSound.setBuffer(this->goombadiedBuffer);
+	this->goombadiedSound.setVolume(2.5f);
 }
 
 void SoundManager::playMarioJump()
 {
 	this->jumpingSound.play();
 }
+
+void SoundManager::playCoin()
+{
+	this->coingSound.play();
+}
+
+void SoundManager::goombadead()
+{
+	this->goombadiedSound.play();
+}
+
+void SoundManager::playerdead()
+{
+	this->dyingSound.play();
+}
+
+void SoundManager::playershrink()
+{
+	this->shrinkSound.play();
+}
+
+void SoundManager::gameover()
+{
+	this->gameoverSound.play();
+}
+
--- SoundManager.h
+++ SoundManager.h
@@ -8,16 +8,40 @@ class SoundManager
 	SoundManager() {};
 	static SoundManager* sm;
 
+	// gameover sound
+	sf::SoundBuffer gameoverBuffer;
+	sf::Sound gameoverSound;
+
+	// jumping sound
 	sf::SoundBuffer jumpingBuffer;
 	sf::Sound jumpingSound;
 
+	// player dying
+	sf::SoundBuffer dyingBuffer;
+	sf::Sound dyingSound;
+
+	// player -hp
+	sf::SoundBuffer shrinkBuffer;
+	sf::Sound shrinkSound;
+
+	// coin sound
+	sf::SoundBuffer coinBuffer;
+	sf::Sound coingSound;
+
+	// goomba ded sound
+	sf::SoundBuffer goombadiedBuffer;
+	sf::Sound goombadiedSound;
 public:
 	SoundManager(SoundManager &sm) = delete;
 	void operator = (const SoundManager &) = delete;
 	static SoundManager* getInstance();
 
-
 	void initSounds();
 	void playMarioJump();
+	void playCoin();
+	void goombadead();
+	void playerdead();
+	void playershrink();
+	void gameover();
 };
 
